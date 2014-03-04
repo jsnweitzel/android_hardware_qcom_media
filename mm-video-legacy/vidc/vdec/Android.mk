8445e7f1895beb3184c67cd23151784ea283e85c
Dragon7780 <Dragon7780@hotmail.nl>
2014-03-05 00:30:01 +0100

    Squashed multiple commits
    add -fno-strict-aliasing flag

--- mm-video-legacy/vidc/vdec/Android.mk
+++ mm-video-legacy/vidc/vdec/Android.mk
@@ -85,6 +85,7 @@ libmm-vdec-inc          += frameworks/native/include/binder
 LOCAL_MODULE                    := libOmxVdec
 LOCAL_MODULE_TAGS               := optional
 LOCAL_CFLAGS                    := $(libOmxVdec-def)
+LOCAL_CFLAGS                    += -fno-strict-aliasing
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 
 LOCAL_PRELINK_MODULE    := false
